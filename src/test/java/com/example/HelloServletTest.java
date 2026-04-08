79d1b7c147cb6d4a35a3df009ba9d972405de886
Spoorthisgowda20s <spoorthisgowda1977@gmail.com>
2026-04-09 00:12:36 +0530

    Update HelloServletTest.java

--- src/test/java/com/example/HelloServletTest.java
+++ src/test/java/com/example/HelloServletTest.java
@@ -2,17 +2,23 @@ package com.example;
 
 import static org.mockito.Mockito.*;
 import java.io.*;
-import javax.io.IOException;
+import org.junit.jupiter.api.Test;
 import javax.servlet.http.*;
 
-@void testDoGet() throws Exception {
+public class HelloServletTest {
+	
+	@Test
+	void testDoGet() throws Exception {
 
 	HttpServlet s = new HttpServlet();
 	HttpServletResponse res = mock(HttpServletResponse.class);
+	HttpServletRequest req = mock(HttpServletRequest.class);
 
 	StringWriter sw = new StringWriter();
 	when(res.getWriter())thenReturn(new PrintWriter(sw));
 
-	s.doGet(res, res);
+	s.doGet(req, res);
 
 	assert(sw.toString().length() > 0);
+	}
+}
