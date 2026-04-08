282ddc86950605b1297c506c0839239b30cbc11f
Spoorthisgowda20s <spoorthisgowda1977@gmail.com>
2026-04-09 00:09:17 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req,
 			     HttpServletResponse res) throws ServletException, IOException {
 
-	res.setContentType("text/html")
+	res.setContentType("text/html");
 	res.getWriter().println("<h2>Hello from serlvet</h2");
 	res.getWriter().println("<p>Github ->Jenkins->Docker</p>");
 		}
